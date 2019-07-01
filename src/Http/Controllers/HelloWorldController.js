2df3d4a5435cd7c575215f6f12fe55ae78344ac1
rikky <son.levuthai@gmail.com>
2019-07-01 12:11:33 +0700

    fixes wrong conditional check

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,12 +1,19 @@
 import {get} from "@fusion.io/framework/Http";
-import {singleton} from "@fusion.io/bare";
+import {singleton, Session} from "@fusion.io/bare";
 import SessionStartMiddleware from "@fusion.io/framework/Session/SessionStartMiddleware";
 
-@singleton()
+@singleton(Session)
 class HelloWorldController {
+
+    constructor(session) {
+        this.session = session;
+    }
 
     @get('/', [SessionStartMiddleware])
     index(context) {
-        context.body = {message: "Hello World"};
+        const count = this.session.get('count', 0);
+
+        this.session.set('count', count + 1);
+        context.body = {message: "Hello World", count};
     }
 }
