f3ecdc0bfb13f6a8aba96f7e304855e609ae2ab6
rikky <son.levuthai@gmail.com>
2019-07-02 12:27:35 +0700

    purge unused code

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,13 +1,12 @@
 import {singleton, middleware} from "@fusion.io/framework";
 import {get} from "@fusion.io/framework/Http/HttpResolver";
-import {Logger} from "@fusion.io/framework/Contracts";
 
 @singleton()
 @middleware('web')
 export default class HelloWorldController {
 
-    @get('/user/:name', Logger)
+    @get('/')
     index(context) {
-        context.body = {message: "Hello World", name: context.params.name};
+        context.body = {message: "Hello World"};
     }
 }
