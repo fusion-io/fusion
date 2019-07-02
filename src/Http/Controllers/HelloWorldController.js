fc4b16860360c2f85bb18799b22c4bc290ff3885
rikky <son.levuthai@gmail.com>
2019-07-02 12:26:27 +0700

    remove unused variables

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,12 +1,12 @@
 import {singleton, middleware} from "@fusion.io/framework";
-import {route, get} from "@fusion.io/framework/Http/HttpResolver";
-import {Logger, Url} from "@fusion.io/framework/Contracts";
+import {get} from "@fusion.io/framework/Http/HttpResolver";
+import {Logger} from "@fusion.io/framework/Contracts";
 
 @singleton()
 @middleware('web')
 export default class HelloWorldController {
 
-    @route('get', '/user/:name', Logger)
+    @get('/user/:name', Logger)
     index(context) {
         context.body = {message: "Hello World", name: context.params.name};
     }
