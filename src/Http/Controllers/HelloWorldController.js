a1d013c8be1fbb8ed87a85856d5562f160d1311a
rikky <son.levuthai@gmail.com>
2019-07-04 01:38:21 +0700

    refactoring database manager registration code for better understanding

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,13 +1,11 @@
 import {get, singleton} from "@fusion.io/framework";
-import {Database} from "@fusion.io/framework/Contracts";
 
 @singleton()
 export default class HelloWorldController {
 
-    @get('/', Database)
-    async index(context, next, database) {
-        const result = await database.connection().raw('select "rikky" as author');
+    @get('/')
+    async index(context) {
 
-        context.body = {...result};
+        context.body = { message: 'Hello world'};
     }
 }
