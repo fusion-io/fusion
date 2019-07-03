6cd0fadd229a9136c943444cf384ccadf672e038
rikky <son.levuthai@gmail.com>
2019-07-03 16:45:14 +0700

    purge the config from the version control system

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,35 +1,38 @@
 import ConfigManager from "@fusion.io/framework/Config/ConfigManager";
 import {container} from "@fusion.io/framework";
 import configValue from "./../config";
-import environmentConfig from "./../config/env";
 import {Config, Kernel} from "@fusion.io/framework/Contracts";
-
-// ---------------------------------------------------------------------------------------------------------------------
-// | Initialize ConfigManager
-// ---------------------------------------------------------------------------------------------------------------------
-// |
-// |
-const config = new ConfigManager(configValue);
-
-container.value(Config, config);
-
-config.setEnv(config.get('env'), environmentConfig[config.get('env')]);
+import path from "path";
+import fs from 'fs';
 
 (async () => {
 
     // -----------------------------------------------------------------------------------------------------------------
-    // | Your initialization tasks
+    // | Initialize ConfigManager
     // -----------------------------------------------------------------------------------------------------------------
-    // | Sometimes, we need to run some tasks to set up the server even before the app bootstrap
-    // | and those tasks might run asynchronously. This is where you can run those tasks.
     // |
-    // | We suggest to write it in a separate file and import it here.
     // |
+    const config = new ConfigManager(configValue);
 
-    // Ex:
-    //
-    //  await myHeavyTask();
+    container.value(Config, config);
 
+    // Loading the environment based config
+    const env           = configValue.env;
+    const configFile    = path.resolve(__dirname + '/../config/env/' + env + '.env.js');
+
+    config.setEnv(env);
+
+    // Try to load the environment config
+    if (fs.existsSync(configFile)) {
+        try {
+            const envConfig = await import(configFile);
+
+            config.merge(envConfig.default);
+        } catch (e) {
+            console.warn(`Warning: Failed to load the environment config for [${env}].`);
+            console.warn(e.stack);
+        }
+    }
 
     // -----------------------------------------------------------------------------------------------------------------
     // | Initialize the bootstrap sequence
@@ -46,6 +49,20 @@ config.setEnv(config.get('env'), environmentConfig[config.get('env')]);
 
     // After the registration has been finished, we'll start bootstrap the services.
     providers.forEach(provider => provider.boot());
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // | Your initialization tasks
+    // -----------------------------------------------------------------------------------------------------------------
+    // | Sometimes, we need to run some tasks to set up the server even before the app bootstrap
+    // | and those tasks might run asynchronously. This is where you can run those tasks.
+    // |
+    // | We suggest to write it in a separate file and import it here.
+    // |
+
+    // Ex:
+    //
+    //  await myHeavyTask(container);
 
 
     // -----------------------------------------------------------------------------------------------------------------
