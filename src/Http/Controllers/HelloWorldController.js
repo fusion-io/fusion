8661f74307bfe0f8d249292e5b1f4bf13ff8eb29
rikky <son.levuthai@gmail.com>
2019-07-05 01:20:05 +0700

    remove test code

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,13 +1,10 @@
 import {get, singleton} from "@fusion.io/framework";
-import {Storage} from "@fusion.io/framework/Contracts";
 
 @singleton()
 export default class HelloWorldController {
 
-    @get('/', Storage)
-    async index(context, next, storage) {
-        // await storage.store('foo', {foo: 'bar'});
-        console.log(await storage.get('foo'));
+    @get('/')
+    async index(context) {
         context.body = { message: 'Hello world'};
     }
 }
