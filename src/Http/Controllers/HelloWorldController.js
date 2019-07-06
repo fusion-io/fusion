00b093ab7f8583052d5c945de2f3e9c716d73be7
rikky <son.levuthai@gmail.com>
2019-07-06 12:31:27 +0700

    better naming convention

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -5,6 +5,15 @@ export default class HelloWorldController {
 
     @get('/')
     async index(context) {
-        context.body = { message: 'Hello world'};
+        // context.body = { message: 'Hello world'};
+        return context.with('message', 'foobar').redirectToRoute("HelloWorldController.hello", {user: 'world'});
+    }
+
+    @get('/hello/:user')
+    async hello(context) {
+        context.body = {
+            hello: context.params.user,
+            message: context.session.get('message')
+        }
     }
 }
