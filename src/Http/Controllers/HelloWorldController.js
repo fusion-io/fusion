75ef03513bf8e8946a6332d8ffe8718d4702f5fc
rikky <son.levuthai@gmail.com>
2019-07-07 00:10:56 +0700

    purge unused code

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,15 +1,10 @@
 import {get, singleton} from "@fusion.io/framework";
-import {Queue} from "@fusion.io/framework/Contracts";
-import LogJob from "../../LogJob";
 
 @singleton()
 export default class HelloWorldController {
 
-    @get('/', Queue)
+    @get('/')
     async index(context) {
-
-        new LogJob('Hello World').dispatch();
-
         return context.body = {
             message: "Hello World"
         };
