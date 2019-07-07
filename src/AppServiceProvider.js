bf0f314a12ed3c7066b10e5cc5b21a01e53bf791
rikky <son.levuthai@gmail.com>
2019-07-07 14:20:51 +0700

    cleanup code

--- src/AppServiceProvider.js
+++ src/AppServiceProvider.js
@@ -1,8 +1,4 @@
 import {ServiceProvider} from "@fusion.io/framework";
-import {QueueRegistry} from "@fusion.io/framework";
-import LogJob from "./LogJob";
-import {View} from "@fusion.io/framework/Contracts";
-
 export default class AppServiceProvider extends ServiceProvider {
 
     register() {
@@ -10,16 +6,6 @@ export default class AppServiceProvider extends ServiceProvider {
     }
 
     boot() {
-        const registry = this.container.make(QueueRegistry);
 
-        registry
-            .register(LogJob)
-        ;
-
-        const view = this.container.make(View);
-
-        view.rendering('welcome', (view) => {
-            view.with('date', new Date());
-        })
     }
 }
