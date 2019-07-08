c7b8f63094f01eb7a75d960ced264c063eb58a32
rikky <son.levuthai@gmail.com>
2019-07-08 09:38:24 +0700

    adding messages for validators

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,5 +1,4 @@
-import {get, singleton, middleware} from "@fusion.io/framework";
-import FooBarForm from "../FooBarForm";
+import {get, singleton} from "@fusion.io/framework";
 
 @singleton()
 export default class HelloWorldController {
@@ -7,18 +6,5 @@ export default class HelloWorldController {
     @get('/')
     index(context) {
         return context.render('welcome');
-    }
-
-    @get('/foobar-form')
-    showForm(context) {
-        context.body = {message: context.session.get('errors')};
-    }
-
-    @get('/handle')
-    @middleware(FooBarForm)
-    handleForm(context) {
-        context.body = {
-            form:  context.foobarForm.value()
-        };
     }
 }
