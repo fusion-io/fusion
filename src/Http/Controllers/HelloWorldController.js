7b453a9b5c3e763f720e6fce73021094289bcd89
rikky <son.levuthai@gmail.com>
2019-07-08 10:22:02 +0700

    adding the session serializing strategy for errors

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,4 +1,5 @@
-import {get, singleton} from "@fusion.io/framework";
+import {get, singleton, middleware} from "@fusion.io/framework";
+import FoobarForm from "../../FoobarForm";
 
 @singleton()
 export default class HelloWorldController {
@@ -6,5 +7,23 @@ export default class HelloWorldController {
     @get('/')
     index(context) {
         return context.render('welcome');
+    }
+
+    @get('/show')
+    show(ctx) {
+        const errors = ctx.errors();
+
+
+        ctx.body = {
+            errors: errors ? errors.translate() : null
+        }
+    }
+
+    @middleware(FoobarForm)
+    @get('/handle')
+    handle(ctx) {
+        ctx.body = {
+            message: 'done'
+        }
     }
 }
