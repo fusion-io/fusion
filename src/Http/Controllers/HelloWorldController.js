84cd4581172e5bb0991b6d0dce9f910183eb38ab
rikky <son.levuthai@gmail.com>
2019-07-15 02:36:07 +0700

    make babel copy the template files

--- src/Http/Controllers/HelloWorldController.js
+++ src/Http/Controllers/HelloWorldController.js
@@ -1,5 +1,4 @@
 import {get, singleton} from "@fusion.io/framework";
-import HelloJob from "../../HelloJob";
 import {Storage} from "@fusion.io/framework/Contracts";
 
 @singleton()
@@ -9,7 +8,6 @@ export default class HelloWorldController {
     async index(context, next, storage) {
         await storage.store('foo', "bar", {tags: ['some', 'tag']});
         await storage.store('foo2', "bar2", {tags: ['some', 'tag', 'here']});
-        await new HelloJob(Date.now()).dispatch();
         return await context.render('welcome');
     }
 }
